97adb12bd7b02c22e7ea77fc9a6f25ad8222336b
bauidch <lukas.bauer@competec.ch>
2017-04-13 13:28:18 +0200

    Create Files

--- roles/uwsgi/files/myapp.py
+++ roles/uwsgi/files/myapp.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+@application.route("/test")
+def test():
+    return "<h1 style='color:blue'>Hey There i am a simply test site/h1>"
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
