126af0e3c8a56d883ea4ba5eb04bc2999ca25060
devdepcomp <teamweb@competec.ch>
2017-04-24 15:40:24 +0200

    PAssenger works

---
+++ roles/passenger/files/myapp.py
@@ -0,0 +1,13 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+@application.route("/test")
+def test():
+    return "<h1 style='color:blue'>Hey There i am a simply test site</h1>"
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
---
+++ roles/passenger/files/passenger_wsgi.py
@@ -0,0 +1,2 @@
+# passenger_wsgi.py
+from myapp import application as application
